d87f6228bd353cd7f925bf4059c2cb8d102b5c6d
LITHIGESH P G <lithigesh@gmail.com>
2025-01-19 22:33:41 +0530

    login timing removed

--- Login/login.js
+++ Login/login.js
@@ -1,21 +1,18 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
   
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-    const message = document.getElementById("message");
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const message = document.getElementById("message");
   
-    if (username === "admin" && password === "admin") {
-      message.className = "alert success";
-      message.textContent = "Login successful!";
-      message.classList.remove("d-none");
-      setTimeout(() => {
-        window.location.href = "../dashboard/dashboard.html";
-      }, 1000);
-    } else {
-      message.className = "alert error";
-      message.textContent = "Incorrect username or password.";
-      message.classList.remove("d-none");
-    }
+  if (username === "admin" && password === "admin") {
+    message.className = "alert success";
+    message.textContent = "Login successful!";
+    message.classList.remove("d-none");
+    window.location.href = "../dashboard/dashboard.html";
+  } else {
+    message.className = "alert error";
+    message.textContent = "Incorrect username or password.";
+    message.classList.remove("d-none");
+  }
   });
-  
